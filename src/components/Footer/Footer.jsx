206c4c78d5eb1072a0c09900a87c7d131f97b2a3
fardin_limbadiya <falimbadiya01@gmail.com>
2024-10-17 18:02:53 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import {
+  
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+import footerLogo from "../../assets/website/logo.png";
+import { FaGithub } from "react-icons/fa6";
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+const Footer = () => {
+  return (
+    <div className="bg-gray-100 dark:bg-gray-950">
+      <section className="container">
+        <div className=" grid md:grid-cols-3 py-5">
+          {/* company Details */}
+          <div className=" py-8 px-4 ">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={footerLogo} alt="Logo" className="max-w-[50px]" />
+              Books Store
+            </h1>
+            <p className="">
+            Providing a diverse collection of books, seamless browsing, and personalized recommendations for every reader.{" "}
+            </p>
+            <br />
+            <div className="flex items-center gap-3">
+              <FaLocationArrow />
+              <p>Mehsana, Gujarat</p>
+            </div>
+            <div className="flex items-center gap-3 mt-3">
+              <FaMobileAlt />
+              <p>+91 123456789</p>
+            </div>
+            {/* Social Handle */}
+            <div className="flex items-center gap-3 mt-6">
+              <a href="https://www.instagram.com/fardin_limbadiya/">
+                <FaInstagram className="text-3xl" />
+              </a>
+              <a href="https://github.com/Fardin-limbadiya">
+                <FaGithub className="text-3xl" />
+              </a>
+              <a href="https://www.linkedin.com/in/fardin-limbadiya-3979ff87/">
+                <FaLinkedin className="text-3xl" />
+              </a>
+            </div>
+          </div>
+          {/* Links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10 ">
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Important Links
+                </h1>
+                <ul className={`flex flex-col gap-3`}>
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:text-primary space-x-1 text-gray-500">
+                      <span>&#11162;</span>
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:text-primary space-x-1 text-gray-500">
+                      <span>&#11162;</span>
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Location
+                </h1>
+                {/* <ul className="list-disc list-inside"> */}
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:text-primary space-x-1 text-gray-500">
+                      <span>&#11162;</span>
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="text-center py-10 border-t-2 border-gray-300/50">
+            @copyright 2024 All rights reserved by 'Fardin Limbadiya'
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
